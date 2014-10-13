f6641c55ab69ffc3c2a323e00baad8c1d34bdcde
Pico <luki_9@wp.pl>
2014-10-13 22:14:49 +0200

    Add basic method

--- conditions/manager.php
+++ conditions/manager.php
@@ -10,6 +10,8 @@
 
 namespace phpbb\autogroups\conditions;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
 * Auto Groups service class
 */
@@ -18,16 +20,50 @@ class manager
 	/** @var array */
 	protected $autogroups_conditions;
 
+	/** @var ContainerInterface */
+	protected $phpbb_container;
+
 	/**
 	* Constructor
 	*
-	* @param array               $autogroups_conditions         Array with auto groups conditions
+	* @param array              $autogroups_conditions      Array with auto groups conditions
+	* @param ContainerInterface $phpbb_container            Service container interface
 	*
 	* @return \phpbb\autogroups\conditions\manager
 	* @access public
 	*/
-	public function __construct($autogroups_conditions)
+	public function __construct($autogroups_conditions, ContainerInterface $phpbb_container)
 	{
 		$this->autogroups_conditions = $autogroups_conditions;
+		$this->phpbb_container = $phpbb_container;
+	}
+
+	/**
+	* Check auto groups conditions and execute them
+	*
+	* @return
+	* @access public
+	*/
+	public function check_conditions()
+	{
+		foreach ($this->autogroups_conditions as $autogroups_condition)
+		{
+			$this->check_condition($autogroups_condition);
+		}
+	}
+
+	/**
+	* Check auto groups condition and execute it
+	*
+	* @param string     $condtion_name      Name of the condition
+	*
+	* @return
+	* @access public
+	*/
+	public function check_condition($condition_name)
+	{
+		$condition = $this->phpbb_container->get($condition_name);
+
+		$condition->check();
 	}
 }
