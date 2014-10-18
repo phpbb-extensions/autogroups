747ab12daf08c0ffa5a1890be748e231e6cc1a96
Pico <luki_9@wp.pl>
2014-10-18 07:08:04 +0200

    Add new methods

--- conditions/manager.php
+++ conditions/manager.php
@@ -23,19 +23,42 @@ class manager
 	/** @var ContainerInterface */
 	protected $phpbb_container;
 
+	/** @var \phpbb\user */
+	protected $user;
+
+	/**
+	* The database table the auto group rules are stored in
+	*
+	* @var string
+	*/
+	protected $autogroups_rules_table;
+
+	/**
+	* The database table the auto group conditions are stored in
+	*
+	* @var string
+	*/
+	protected $autogroups_condition_types_table;
+
 	/**
 	* Constructor
 	*
-	* @param array              $autogroups_conditions      Array with auto groups conditions
-	* @param ContainerInterface $phpbb_container            Service container interface
+	* @param array              $autogroups_conditions              Array with auto groups conditions
+	* @param ContainerInterface $phpbb_container                    Service container interface
+	* @param \phpbb\user        $user                               User object
+	* @param string             $autogroups_rules_table             Name of the table used to store auto group rules data
+	* @param string             $autogroups_condition_types_table   Name of the table used to store auto group conditions data
 	*
 	* @return \phpbb\autogroups\conditions\manager
 	* @access public
 	*/
-	public function __construct($autogroups_conditions, ContainerInterface $phpbb_container)
+	public function __construct($autogroups_conditions, ContainerInterface $phpbb_container, \phpbb\user $user, $autogroups_rules_table, $autogroups_condition_types_table)
 	{
 		$this->autogroups_conditions = $autogroups_conditions;
 		$this->phpbb_container = $phpbb_container;
+		$this->user = $user;
+		$this->autogroups_rules_table = $autogroups_rules_table;
+		$this->autogroups_condition_types_table = $autogroups_condition_types_table;
 	}
 
 	/**
@@ -65,5 +88,79 @@ class manager
 		$condition = $this->phpbb_container->get($condition_name);
 
 		$condition->check();
+	}
+
+	/**
+	* Add new condition for Auto Groups extension
+	*
+	* @param string     $condition_type_name      Type identifier of the condition
+	*
+	* @return
+	* @access public
+	*/
+	public function add_condition($condition_type_name)
+	{
+		$sql = 'INSERT INTO ' . $this->autogroups_condition_types_table . ' condition_type_name = ' . $condition_type_name;
+		$this->db->sql_query($sql);
+	}
+
+	/**
+	* Purge all conditions of a certain type
+	*
+	* @param string     $condition_type_name      Type identifier of the condition
+	*
+	* @return
+	* @access public
+	*/
+	public function purge_condition($condition_type_name)
+	{
+		try
+		{
+			$condtion_type_id = $this->get_condition_type_id($condition_type_name);
+
+			$sql = 'DELETE FROM ' . $this->autogroups_rules_table . '
+				WHERE condition_type_id = ' . (int) $condtion_type_id;
+			$this->db->sql_query($sql);
+
+			$sql = 'DELETE FROM ' . $this->autogroups_condition_types_table . '
+				WHERE condition_type_id = ' . (int) $condtion_type_id;
+			$this->db->sql_query($sql);
+		}
+		catch (\phpbb\autogroups\exception $e)
+		{
+			// Continue
+		}
+	}
+
+	/**
+	* Get the condition type id from the name
+	*
+	* @param string     $condition_type_name      Type identifier of the condition
+	*
+	* @return int The condition_type_id
+	* @throws \phpbb\autogroups\exception
+	*/
+	public function get_condition_type_id($condition_type_name)
+	{
+		$condition_type_ids = array();
+
+		$sql = 'SELECT condition_type_id, condition_type_name
+			FROM ' . $this->autogroups_condition_types_table;
+		$result = $this->db->sql_query($sql);
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$condition_type_ids[$row['condition_type_name']] = (int) $row['condition_type_id'];
+		}
+		$this->db->sql_freeresult($result);
+
+		if (!isset($condition_type_ids[$condition_type_name]))
+		{
+			if (!isset($this->autogroups_conditions[$condition_type_name]))
+			{
+				throw new \phpbb\autogroups\exception($this->user->lang('CONDITION_TYPE_NOT_EXIST', $condition_type_name));
+			}
+		}
+
+		return $condition_type_ids[$condition_type_name];
 	}
 }
