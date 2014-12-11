76f0073e5d80a3334b32cc50f4bd6ebd079adaba
Matt Friedman <maf675@gmail.com>
2014-12-10 20:13:08 -0800

    Add notification classes

---
+++ notification/type/group_added.php
@@ -0,0 +1,113 @@
+<?php
+/**
+*
+* Auto Groups extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\autogroups\notification\type;
+
+class group_added extends \phpbb\notification\type\base
+{
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_type()
+	{
+		return 'phpbb.autogroups.notification.type.group_added';
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function is_available()
+	{
+		return false;
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public static function get_item_id($data)
+	{
+		return (int) $data['group_id'];
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public static function get_item_parent_id($data)
+	{
+		return 0;
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function find_users_for_notification($data, $options = array())
+	{
+		$users = array();
+
+		$data['user_ids'] = (!is_array($data['user_ids'])) ? array($data['user_ids']) : $data['user_ids'];
+
+		foreach ($data['user_ids'] as $user_id)
+		{
+			$users[$user_id] = array('');
+		}
+
+		return $users;
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function users_to_query()
+	{
+		return array();
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_title()
+	{
+		return $this->user->lang('NOTIFICATION_AUTOGROUPS_GROUP_ADDED', $this->get_data('group_name'));
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_url()
+	{
+		return append_sid($this->phpbb_root_path . 'memberlist.' . $this->php_ext, "mode=group&g={$this->item_id}");
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_email_template()
+	{
+		return false;
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_email_template_variables()
+	{
+		return array();
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function create_insert_array($data, $pre_create_data = array())
+	{
+		$this->set_data('group_name', $data['group_name']);
+
+		return parent::create_insert_array($data, $pre_create_data);
+	}
+}
