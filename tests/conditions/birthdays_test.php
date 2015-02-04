21456195a1163854b4fe8cf2a0dfb708d46753da
Matt Friedman <maf675@gmail.com>
2015-02-04 15:52:07 -0800

    Update tests

--- tests/conditions/birthdays_test.php
+++ tests/conditions/birthdays_test.php
@@ -13,29 +13,37 @@ namespace phpbb\autogroups\tests\conditions;
 /**
  * Run tests on the birthdays type class.
  */
-class birthdays_test extends base
+class birthdays_test extends type_test_case
 {
 	protected $condition_type = 'phpbb.autogroups.type.birthdays';
 
 	public function get_condition()
 	{
-		return new \phpbb\autogroups\conditions\type\birthdays($this->phpbb_container, $this->config, $this->db, $this->user, 'phpbb_autogroups_rules', 'phpbb_autogroups_types', $this->root_path, $this->php_ext);
+		return new \phpbb\autogroups\conditions\type\birthdays(
+			$this->phpbb_container,
+			$this->config,
+			$this->db,
+			$this->user,
+			'phpbb_autogroups_rules',
+			'phpbb_autogroups_types',
+			$this->root_path,
+			$this->php_ext
+		);
 	}
 
 	/**
-	 * Data for test_check_birthdays
+	 * Mock settings in the database:
+	 * Ages under 20 adds to group 2 as default
+	 * Ages between 30 - 50 adds to group 3 (no default)
+	 * Ages over 40 adds to group 4 (no default)
+	 *
+	 * User 1 is already a member of groups 1 and 5 (1 is default)
+	 * User 2 is already a member of groups 1 and 2 (2 is default)
+	 *
+	 * @return Array of test data
 	 */
-	public function check_birthdays_test_data()
+	public function check_condition_test_data()
 	{
-		/*
-		 * Mock settings in the database:
-		 * Ages under 20 adds to group 2 as default
-		 * Ages between 30 - 50 adds to group 3 (no default)
-		 * Ages over 40 adds to group 4 (no default)
-		 *
-		 * User 1 is already a member of groups 1 and 5 (1 is default)
-		 * User 2 is already a member of groups 1 and 2 (2 is default)
-		 */
 		return array(
 			array(
 				array(
@@ -50,8 +58,10 @@ class birthdays_test extends base
 					1 => 2, // default
 					2 => 1, // default
 				),
+				array(),
 			),
 			array(
+				array(),
 				array(),
 				array(),
 				array(),
@@ -60,106 +70,35 @@ class birthdays_test extends base
 	}
 
 	/**
-	 * Test the birthdays check method
+	 * Data for test_check_group_exemptions
 	 *
-	 * @dataProvider check_birthdays_test_data
+	 * @return Array of test data
 	 */
-	public function test_check_birthdays($user_data, $expected, $default)
+	public function check_group_exemptions_data()
 	{
-		// Update the users birthdays
-		foreach ($user_data as $user_id => $age)
-		{
-			$this->helper_update_user_birthdays($user_id, $age);
-		}
-
-		// Instantiate the birthdays condition
-		$condition = $this->get_condition();
-
-		// Get the users to check by default
-		$users = $condition->get_users_for_condition();
-
-		// Perform the check on the users
-		$condition->check($users);
-
-		// Assert the user's groups are as expected
-		$result = $condition->get_users_groups(array_keys($user_data));
-		foreach ($result as $key => $ary)
-		{
-			sort($result[$key]);
-		}
-		$this->assertEquals($expected, $result);
-
-		// Assert the user's default group id is as expected
-		$this->assertEquals($default, $this->helper_default_groups(array_keys($user_data)));
+		return array(
+			array(
+				1, // user id
+				10, // Age 10 (would make group 2 the new default)
+				1, // default exempt group
+			),
+		);
 	}
 
 	/**
-	 * Test the birthdays check method by passing it the user ids
+	 * Update the database with new registration timestamp user
 	 *
-	 * @dataProvider check_birthdays_test_data
+	 * @param int $user_id
+	 * @param int $data The age in years of a user
 	 */
-	public function test_check_birthdays_alt($user_data, $expected, $default)
-	{
-		// Update the users regdate timestamp
-		foreach ($user_data as $user_id => $age)
-		{
-			$this->helper_update_user_birthdays($user_id, $age);
-		}
-
-		// Instantiate the birthdays condition
-		$condition = $this->get_condition();
-
-		// Get the users to check passing user ids manually
-		$users = $condition->get_users_for_condition(array(
-			'users' => array_keys($user_data),
-		));
-
-		// Perform the check on the users
-		$condition->check($users);
-
-		// Assert the user's groups are as expected
-		$result = $condition->get_users_groups(array_keys($user_data));
-		foreach ($result as $key => $ary)
-		{
-			sort($result[$key]);
-		}
-		$this->assertEquals($expected, $result);
-
-		// Assert the user's default group id is as expected
-		$this->assertEquals($default, $this->helper_default_groups(array_keys($user_data)));
-	}
-
-	/*
-	 * Update the database with user ages (relative to current time)
-	 */
-	public function helper_update_user_birthdays($user_id, $age)
+	public function helper_update_user_data($user_id, $data)
 	{
 		$now = getdate(time());
-		$birthday = sprintf('%2d-%2d-%4d', $now['mday'], $now['mon'], ($now['year'] - $age));
+		$birthday = sprintf('%2d-%2d-%4d', $now['mday'], $now['mon'], ($now['year'] - $data));
 
 		$sql = "UPDATE phpbb_users
 			SET user_birthday = '" . $this->db->sql_escape($birthday) . "'
 			WHERE user_id = " . (int) $user_id;
 		$this->db->sql_query($sql);
-	}
-
-	/*
-	 * Get the default groups for users
-	 */
-	public function helper_default_groups($user_ids)
-	{
-		$sql = 'SELECT user_id, group_id
-			FROM phpbb_users
-			WHERE ' . $this->db->sql_in_set('user_id', $user_ids, false, true);
-		$result = $this->db->sql_query($sql);
-
-		$rowset = array();
-		while ($row = $this->db->sql_fetchrow($result))
-		{
-			$rowset[$row['user_id']] = $row['group_id'];
-		}
-		$this->db->sql_freeresult($result);
-
-		return $rowset;
 	}
 }
