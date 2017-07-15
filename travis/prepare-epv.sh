534d3ad3afdd81105c2bd0f3dba2046eaf7962c2
Matt Friedman <maf675@gmail.com>
2017-07-15 09:09:43 -0500

    Ignore config platform

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -17,6 +17,6 @@ NOTESTS=$2
 if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
